d1ccf011e31854fa9e687ecf2208fc094da0c757
Forsee41 <forsee41@gmail.com>
2023-08-03 11:58:56 +0300

    add notifier state base

--- src/handler.rs
+++ src/handler.rs
@@ -1,12 +1,8 @@
-use serenity::{
-    async_trait,
-    model::gateway::Ready,
-    prelude::*,
-};
+use crate::notifier::Notifier;
+use serenity::{async_trait, model::gateway::Ready, prelude::*};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio_cron_scheduler::{Job, JobScheduler};
-use crate::notifier::Notifier;
 
 pub struct Handler {
     channel_id: u64,
@@ -24,13 +20,13 @@ impl Handler {
     }
 }
 
-
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("Bot is ready, user: {}", ready.user.name);
         let ctx_arc = Arc::new(ctx);
-        let notifier = Notifier::new(ctx_arc, self.channel_id);
+        let bot_uid = ready.user.id;
+        let notifier = Notifier::new(ctx_arc, self.channel_id, bot_uid).await;
         *self.notifier.lock().await = Some(notifier);
         let messages = self
             .notifier
@@ -41,8 +37,11 @@ impl EventHandler for Handler {
             .get_messages()
             .await;
         println!("{}", messages[0].content);
-        let job = Job::new_async("* * * * * *", |_uuid, _l| {
+        let notifier_clone = Arc::clone(&self.notifier);
+        let job = Job::new_async("* * * * * *", move |_uuid, _l| {
+            let notifier_clone = Arc::clone(&notifier_clone);
             Box::pin(async move {
+                notifier_clone.lock().await.as_mut().unwrap().notify().await;
                 println!("Doing a job");
             })
         })
--- src/notifier.rs
+++ src/notifier.rs
@@ -1,18 +1,85 @@
+use chrono::{prelude::*, Duration};
 use serenity::{
-    model::{channel::Message, id::ChannelId},
+    async_trait,
+    model::{channel::Message, id::ChannelId, prelude::UserId},
     prelude::*,
 };
 use std::sync::Arc;
 
-enum ChannelState {
-    Uninitialized,
-    Notified,
-    NotNotified,
+#[async_trait]
+trait State: Send + Sync {
+    async fn update_time(&self, notifier: &Notifier) -> ();
+    async fn notify(&self, notifier: &Notifier) -> ();
+    async fn unnotify(&self, notifier: &Notifier) -> ();
+}
+
+struct Uninitialized;
+
+#[async_trait]
+impl State for Uninitialized {
+    async fn update_time(&self, notifier: &Notifier) -> () {}
+    async fn notify(&self, notifier: &Notifier) -> () {
+        println!("Notifying users!");
+    }
+    async fn unnotify(&self, notifier: &Notifier) -> () {}
+}
+
+struct Notified;
+
+#[async_trait]
+impl State for Notified {
+    async fn update_time(&self, notifier: &Notifier) -> () {}
+    async fn notify(&self, notifier: &Notifier) -> () {}
+    async fn unnotify(&self, notifier: &Notifier) -> () {}
+}
+
+struct NonNotified;
+
+#[async_trait]
+impl State for NonNotified {
+    async fn update_time(&self, notifier: &Notifier) -> () {}
+    async fn notify(&self, notifier: &Notifier) -> () {}
+    async fn unnotify(&self, notifier: &Notifier) -> () {}
+}
+
+struct MessageManager {
+    start: u64,
+    end: u64,
+    shift: u64,
+    tz_shift: u64,
+}
+impl MessageManager {
+    fn now_tz(&self) -> DateTime<FixedOffset> {
+        let tz = self.tz();
+        let utc_now = Utc::now();
+        utc_now.with_timezone(&tz)
+
+    }
+    fn tz(&self) -> FixedOffset {
+        let hour = 3600;
+        let tz_shift_seconds: i32 = hour * self.tz_shift as i32;
+        FixedOffset::east_opt(tz_shift_seconds).unwrap()
+    }
+    fn next_hh_start_datetime(&self) -> DateTime<FixedOffset> {
+        let now = self.now_tz();
+        let hh_start = now.clone().date_naive().and_hms_opt(self.start as u32, 0, 0).unwrap();
+        let hh_start_with_tz: DateTime<FixedOffset> = DateTime::<FixedOffset>::from_utc(hh_start, self.tz()).with_timezone(&self.tz());
+        if now.hour() as u64 >= self.start {
+            hh_start_with_tz + Duration::days(1)
+        } else {
+            hh_start_with_tz
+        }
+    }
+    fn duration_until_next_hh_start(&self) {}
+    async fn generate_info_str(&self) -> String {
+        "".to_owned()
+    }
 }
 
 pub struct Notifier {
     ctx: Arc<Context>,
     channel_id: u64,
+    bot_uid: UserId,
 }
 
 impl Notifier {
@@ -24,9 +91,41 @@ impl Notifier {
             .expect("Failed to fetch messages")
     }
 
-    pub async fn get_state() {}
+    async fn state(&self, messages: &Vec<Message>) -> Box<dyn State> {
+        if (1..=2).contains(&messages.len()) {
+            return Box::new(Uninitialized);
+        };
+        for msg in messages {
+            if msg.author.id != self.bot_uid {
+                return Box::new(Uninitialized);
+            }
+        }
+        Box::new(NonNotified)
+    }
 
-    pub fn new(ctx: Arc<Context>, channel_id: u64) -> Notifier {
-        Notifier { ctx, channel_id }
+    pub async fn update_time(&mut self) {
+        let messages = self.get_messages().await;
+        let state = self.state(&messages).await;
+        state.update_time(self).await;
+    }
+
+    pub async fn notify(&mut self) {
+        let messages = self.get_messages().await;
+        let state = self.state(&messages).await;
+        state.notify(self).await;
+    }
+
+    pub async fn unnotify(&mut self) {
+        let messages = self.get_messages().await;
+        let state = self.state(&messages).await;
+        state.unnotify(self).await;
+    }
+
+    pub async fn new(ctx: Arc<Context>, channel_id: u64, bot_uid: UserId) -> Notifier {
+        Notifier {
+            ctx,
+            channel_id,
+            bot_uid,
+        }
     }
 }
